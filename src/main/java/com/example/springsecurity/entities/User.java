8f25c87565a1ae8c51ba71a10f15a348257b7162
Morgan Ogola <morganogola@gmail.com>
2024-02-07 14:17:17 +0300

    Added JWTAuthenticationFilter

--- src/main/java/com/example/springsecurity/entities/User.java
+++ src/main/java/com/example/springsecurity/entities/User.java
@@ -1,24 +1,28 @@
 package com.example.springsecurity.entities;
-
 import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-
 import java.util.Collection;
 import java.util.List;
 
-@Data
+
 @Entity
-@Table
+@Data
+@Table(name="\"user\"")
 public class User implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
     private Integer id;
+    @Column(name ="firstname")
     private String firstname;
+    @Column(name = "lastname")
     private String lastname;
+    @Column(name ="email")
     private String email;
+    @Column(name = "password")
     private String password;
 
     private Role role;
--- src/main/java/com/example/springsecurity/services/JWTService.java
+++ src/main/java/com/example/springsecurity/services/JWTService.java
@@ -1,4 +1,9 @@
 package com.example.springsecurity.services;
 
+import org.springframework.security.core.userdetails.UserDetails;
+
 public interface JWTService {
+    String extractUserName(String token);
+
+    String generateToken(UserDetails userDetails);
 }
