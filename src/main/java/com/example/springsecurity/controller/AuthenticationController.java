01df67e63c127793f201b307fa9f9df8facc0094
Morgan Ogola <morganogola@gmail.com>
2024-02-08 14:57:01 +0300

    Added a Security Configuration and added admin and user roles to access their respective endpoints

---
+++ src/main/java/com/example/springsecurity/controller/AuthenticationController.java
@@ -0,0 +1,10 @@
+package com.example.springsecurity.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+@RestController
+public class AuthenticationController {
+}
